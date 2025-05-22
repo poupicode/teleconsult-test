80565771d6dbe15a8280f4176123c597e67c382a
Magali MAI <maimagali26.pro@gmail.com>
2025-05-22 16:46:40 +0200

    style: :lipstick: Add Header components with public and dashboard variants + Stylize the home page
    Public variant for home and login/register pages and dashboard variant for consultation page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { useAuth } from "@/contexts/AuthContext";
+import Header from "@/components/Header";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -11,32 +12,44 @@ function HomePage() {
   };
 
   return (
-    <div className="container mt-5 w-100">
-      <h1>Bienvenue</h1>
-      {session ? (
-        <>
-          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quod cupiditate error, omnis at aliquid? Possimus pariatur maiores, recusandae obcaecati, a enim distinctio consequatur cumque sapiente modi optio aut? Rerum!</p>
-          <Button
-            variant="primary"
-            onClick={() => navigate("/consultation")}
-            className="mt-3 primary-btn mx-auto d-block"
-          >
-            Accéder à la consultation
-          </Button>
-        </>
-      ) : (
-        <>
-          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quod cupiditate error, omnis at aliquid? Possimus pariatur maiores, recusandae obcaecati, a enim distinctio consequatur cumque sapiente modi optio aut? Rerum!</p>
-          <Button
-            variant="primary"
-            onClick={handleLoginClick}
-            className="mt-5 primary-btn mx-auto d-block fs-5"
-          >
-            Commencer
-          </Button>
-        </>
-      )}
-    </div>
+    <>
+      <Header variant="public" title="Bienvenue" />
+      <div className="container ps-5 pe-5 mt-2 w-100">
+        {session ? (
+          <>
+            <p className="text-center w-50 mx-auto mt-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+              quod cupiditate error, omnis at aliquid? Possimus pariatur
+              maiores, recusandae obcaecati, a enim distinctio consequatur
+              cumque sapiente modi optio aut? Rerum!
+            </p>
+            <Button
+              variant="primary"
+              onClick={() => navigate("/consultation")}
+              className="mt-5 primary-btn mx-auto d-block"
+            >
+              Accéder à la consultation
+            </Button>
+          </>
+        ) : (
+          <>
+            <p className="text-center w-50 mx-auto mt-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+              quod cupiditate error, omnis at aliquid? Possimus pariatur
+              maiores, recusandae obcaecati, a enim distinctio consequatur
+              cumque sapiente modi optio aut? Rerum!
+            </p>
+            <Button
+              variant="primary"
+              onClick={handleLoginClick}
+              className="mt-5 primary-btn mx-auto d-block fs-5"
+            >
+              Commencer
+            </Button>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
