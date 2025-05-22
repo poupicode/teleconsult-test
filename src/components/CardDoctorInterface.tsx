f9e05d8fe901696f1d6ec1ae1210e10e4d8eb71c
Erwan V <erwan.vasselin16@gmail.com>
2025-05-22 16:58:00 +0200

    card for choose/delete the consultation room

---
+++ src/components/CardDoctorInterface.tsx
@@ -0,0 +1,30 @@
+export default function CardDoctorInterface() {
+  return (
+    <div
+      className="card shadow-lg p-3 mb-4 rounded"
+      style={{ width: '25%', backgroundColor: '#F0EDF4' }}
+    >
+      <div className="card-body">
+        <h5 className="card-title">Salle :</h5>
+
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          maxime at tenetur sapiente quos perferendis!
+        </p>
+
+        <button type="button" className="btn btn-danger rounded-pill mx-auto d-block mb-3 px-5">
+          Rejoindre
+        </button>
+
+        <hr className="my-3 border border-primary" />
+
+        <div className="d-flex justify-content-center">
+            <button type="button" className="btn btn-link text-danger">
+             Supprimer la salle
+            </button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,6 @@ import { Button } from "react-bootstrap";
 import { useAuth } from "@/contexts/AuthContext";
 import Header from "@/components/Header";
 
-
-
-
-
 function HomePage() {
   const navigate = useNavigate();
   const { session } = useAuth();
