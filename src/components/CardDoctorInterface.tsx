7c86e8d72840d055c5f4b13fae35a8e3a4248bff
Erwan V <erwan.vasselin16@gmail.com>
2025-05-22 16:58:00 +0200

    card for choose/delete the consultation room

--- src/components/CardDoctorInterface.tsx
+++ src/components/CardDoctorInterface.tsx
@@ -1,35 +1,31 @@
-import react from "react"
-
-interface CardDoctorInterfaceProps {
-  name: string;
-  code: string;
-}
-
-export default function CardDoctorInterface({name, code}: CardDoctorInterfaceProps) {
+export default function CardDoctorInterface() {
   return (
     <div
       className="card shadow-lg p-3 mb-4 rounded"
-      style={{ backgroundColor: '#F0EDF4' }}
+      style={{ width: "25%", backgroundColor: "#F0EDF4" }}
     >
       <div className="card-body">
-        <h5 className="card-title">Salle : {name}</h5>
+        <h5 className="card-title">Salle :</h5>
 
         <p className="card-text">
-          {code}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          maxime at tenetur sapiente quos perferendis!
         </p>
 
-        <button type="button" className="btn btn-danger rounded-pill mx-auto d-block mb-3 px-5">
+        <button
+          type="button"
+          className="btn btn-danger rounded-pill mx-auto d-block mb-3 px-5"
+        >
           Rejoindre
         </button>
 
         <hr className="my-3 border border-primary" />
 
         <div className="d-flex justify-content-center">
-            <button type="button" className="btn btn-link text-danger">
-             Supprimer la salle
-            </button>
+          <button type="button" className="btn btn-link text-danger">
+            Supprimer la salle
+          </button>
         </div>
-
       </div>
     </div>
   );
