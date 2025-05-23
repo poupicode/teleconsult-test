5443e1a75e01c7069e5b250ca1847380faad58c8
Erwan V <erwan.vasselin16@gmail.com>
2025-05-23 14:24:56 +0200

    WIP for the page where you choose/creat/delete de room for the teleconsult

--- src/components/CardDoctorInterface.tsx
+++ src/components/CardDoctorInterface.tsx
@@ -1,16 +1,23 @@
-export default function CardDoctorInterface() {
+import react from "react";
+
+interface CardDoctorInterfaceProps {
+  name: string;
+  code: string;
+}
+
+export default function CardDoctorInterface({
+  name,
+  code,
+}: CardDoctorInterfaceProps) {
   return (
     <div
       className="card shadow-lg p-3 mb-4 rounded"
-      style={{ width: "25%", backgroundColor: "#F0EDF4" }}
+      style={{ backgroundColor: "#F0EDF4" }}
     >
       <div className="card-body">
-        <h5 className="card-title">Salle :</h5>
+        <h5 className="card-title">Salle : {name}</h5>
 
-        <p className="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-          maxime at tenetur sapiente quos perferendis!
-        </p>
+        <p className="card-text">{code}</p>
 
         <button
           type="button"
