3ac700e048927fe325029d8d54d294cf602f5a56
Guerric Cochelin <cochelin.guerric@hotmail.com>
2025-06-04 14:30:45 +0200

    receiveData correction v2 for extraction payload

--- src/features/bluetooth/useDoctorData.ts
+++ src/features/bluetooth/useDoctorData.ts
@@ -8,10 +8,13 @@ export function useDoctorData() {
   const [doctorServices, setDoctorServices] = useState<DoctorServices>({});
   const [newData, setNewData] = useState<object | null>(null);
 
-  const receiveData = (rawDataReceived: object) => {
-    console.log('[Médecin] Mesure reçue :', rawDataReceived);
-    setNewData(rawDataReceived);
-  };
+  const receiveData = (rawDataReceived: any) => {
+  const payload = rawDataReceived.payload;
+  if (!payload) return;
+  console.log('[Médecin] Payload reçu :', payload);
+  setNewData(payload); // ✅ on n’envoie QUE les données utiles à processNewData
+};
+
 
   const processNewData = (currentData: object) => {
     const service = Object.keys(currentData)[0];
