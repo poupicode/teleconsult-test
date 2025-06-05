1b29ac6ade2eabdd781ac9247991d5f12dce6aa3
poupicode <gillesmarchesse@gmail.com>
2025-06-05 16:56:44 +0200

    remove relay

--- src/features/room/rtc/ice/ice-config-slice.ts
+++ src/features/room/rtc/ice/ice-config-slice.ts
@@ -23,7 +23,7 @@ export const DEFAULT_ICE_CONFIG: RTCConfiguration = {
         }
     ],
     "iceCandidatePoolSize": 20,
-    iceTransportPolicy: 'relay'
+    // iceTransportPolicy: 'relay'
     // "iceTransportPolicy": "relay" as RTCIceTransportPolicy 
     // Cette option peut être retirée si besoin pour revenir au mode par défaut
 };
